513c4954830759bb54d8026f758f45154bedb124
salazarweb <davidsalazar.web@gmail.com>
2024-05-18 03:00:43 -0400

    update changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import { Filters } from './Filters.jsx';
 export function Header () {
     return (
         <header>
-            <h1>Ecommerce 🛒</h1>
             <Filters />
         </header>
     )
